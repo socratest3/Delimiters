2a3e7494eab77a1dedab833e903bead3ed260081
socratest3 <socratest3@nycstudents.net>
2025-05-04 11:46:25 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,14 @@
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        String[] tokens = {"(", "x + y", ")", " * 5"};
+        Delimiters d = new Delimiters("(", ")");
+        System.out.println(d.getDelimitersList(tokens)); // Output: [(, )]
+        System.out.println(d.isBalanced(d.getDelimitersList(tokens))); // Output: true
 
+        String[] something = {"<q>", "yy", "</q>", "zz", "</q>"};
+        Delimiters a = new Delimiters("<q>", "</q>");
+        System.out.println(a.isBalanced(a.getDelimitersList(something))); // Output: false
+    }
+}
